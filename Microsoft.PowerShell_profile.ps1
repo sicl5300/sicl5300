f8ce20e656025610c9df52d5a42a7aa78868f77c
Sicl <47028479+sicl5300@users.noreply.github.com>
2023-10-21 17:11:14 +0800

    `Microsoft.PowerShell_profile.ps1`

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,27 @@
+oh-my-posh init pwsh --config '~\AppData\Local\Programs\oh-my-posh\themes\material.omp.json' | Invoke-Expression
+Import-Module posh-git
+
+chcp 65001
+
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+# Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Write-Output -InputObject "Hello, $env:USERNAME at $(pwd.exe)."
+
+function lvlme {
+    if ($(whoami /groups) -match "S-1-16-8192") {
+        Write-Output "Medium Mandatory Level"
+    } elseif ($(whoami /groups) -match "S-1-16-12288") {
+        Write-Output "High Mandatory Level"
+    }
+}
+
+## End PROFILE ##
+
+# Aliases
+
+function __ll_function_alias {
+    ls.exe -al
+}
+
+New-Alias -Name ll -Value __ll_function_alias;
